4b6f45058bc624d01a8b0dd3ac95824b1106199e
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2021-09-29 16:40:26 +0200

    abstracting and changing frame

--- PasswordManager-Frontend/main.py
+++ PasswordManager-Frontend/main.py
@@ -4,11 +4,57 @@ import tkinter.ttk as ttk
 
 #region Button Functions
 
+def Add():
+    print("Added")
+
 def Enter():
-    if backend.CheckPass(loginEntry.get()):
+    if backend.CheckPass(login.entry.get()):
         print("Entered")
+        login.destroy()
+        main = Main(root)
     else:
         print("Wrong password")
+
+#endregion
+
+#region Classes
+
+class Login(ttk.Frame):
+    def __init__(self, parent):
+
+        # Creating elements
+        ttk.Frame.__init__(self, parent)
+        self.entry = ttk.Entry(self, show="\u2022")
+        self.label = ttk.Label(self, text="Password Manager", font=('Segoe UI Semibold', 40))
+        self.button = ttk.Button(self, text="Enter", command=Enter)
+
+        # Adding elements
+        self.grid(column=0, row=0)
+        self.label.grid(column=0, row=0, columnspan=2, sticky='s')
+        self.entry.grid(column=0, row=1, sticky='e')
+        self.button.grid(column=1, row=1, sticky='w')
+
+        # Configuring rows and columns
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+
+class Main(ttk.Frame):
+    def __init__(self, parent):
+
+        # Creating elements
+        ttk.Frame.__init__(self, parent)
+        self.search = ttk.Entry(self)
+        self.label = ttk.Label(self, text="LOL", font=('Segoe UI Bold', 50))
+        self.add = ttk.Button(self, text="+", command=Add)
+        
+        # Adding elements
+        self.grid(column=0, row=0)
+
+        self.search.pack()
+        self.label.pack()
+        self.add.pack()
 
 #endregion
 
@@ -24,47 +70,6 @@ if __name__ == '__main__':
     root.rowconfigure(0, weight=1)
     root.columnconfigure(0, weight=1)
 
-    ## Login Frame
-    login = ttk.Frame(root)
+    login = Login(root)
 
-    # Creating elements
-    loginLabel = ttk.Label(login, text="Password Manager", font=('Segoe UI Semibold', 40))
-    loginEntry = ttk.Entry(login, show="\u2022")
-    loginButton = ttk.Button(login, text="Enter", command=Enter)
-
-    # Adding elements
-    login.grid(column=0, row=0)
-    loginLabel.grid(column=0, row=0, columnspan=2, sticky='s')
-    loginEntry.grid(column=0, row=1, sticky='e')
-    loginButton.grid(column=1, row=1, sticky='w')
-
-    # Configuring rows and columns
-    login.rowconfigure(0, weight=1)
-    login.rowconfigure(1, weight=1)
-
-    login.columnconfigure(0, weight=1)
-    login.columnconfigure(1, weight=1)
-
-    #root.columnconfigure(0, weight=1)
-    #root.columnconfigure(1, weight=2)
-    #root.columnconfigure(2, weight=5)
-    #
-    ##  Rows
-    #root.rowconfigure(0, weight=1)
-    #root.rowconfigure(1, weight=2)
-    #root.rowconfigure(2, weight=3)
-    #
-    ## Creating label
-    #label1 = ttk.Label(root, text="Password Manager", font=('Segoe UI Semibold', 40))
-    #label1.grid(row=1, column=1, columnspan=2, sticky='s')
-    #
-    ## Creating Input Field
-    #input = ttk.Entry(root, width=40, show="\u2022") # u2022 is a bullet point
-    #input.grid(row=2, column=1, sticky='ne')
-    #
-    ## Creating button
-    #enter = ttk.Button(root, text="Enter", command=Enter)
-    #enter.grid(row=2, column=2, sticky='nw')
-    #
-    # Running program
     root.mainloop()
